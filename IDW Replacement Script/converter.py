e5f3b5ade93eb2c1f081ee2b3b78c5314120af81
Jake Ratcliffe <JakeR1342@gmail.com>
2022-02-09 13:46:17 +0000

    Added working script file and updated csv

---
+++ IDW Replacement Script/converter.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import json
+
+df = pd.read_csv("./EnginebAuditCDMv1.8 + Descriptions.csv")
+df = df.rename(columns={
+    "enginebAttribute": "name",
+    "MS_DataType": "dataType",
+    "primaryKey": "isNullable",
+    "Description": "description"
+})
+
+df["isNullable"] = np.where(df["isNullable"].isnull(), "true", "false")
+df["displayName"] = df["name"]
+df["decimalPlaces"] = np.where(df["dataType"] == "decimal", 5, None)
+
+df_grouped = df.groupby("enginebEntity")
+
+for name, group in df_grouped:
+    json_object = {
+        "jsonSchemaSemanticVersion": "1.0.0",
+        "imports": [
+            {
+                "corpusPath": "_allImports.cdm.json"
+            }
+        ],
+        "definitions": [
+            {
+                "entityName": name,
+                "extendsEntity": "CdmEntity",
+                "hasAttributes": [
+                    json.loads(group[["name", "dataType", "isNullable", "displayName", "decimalPlaces", "description"]].to_json(orient="records"))
+                ]
+            }
+        ]
+    }
+    
+    with open(f"output/{name}.cdm.json", "w") as outfile:
+        json.dump(json_object, outfile, indent=4)
